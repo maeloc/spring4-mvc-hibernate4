e31b19e0ef3f6895a5a15da021bab4e64096be86
maeloc <anxogomez@gmail.com>
2018-04-29 19:54:29 +0200

    Create views folder and move jsp to it

--- src/main/java/com/websystique/springmvc/controller/EmployeeController.java
+++ src/main/java/com/websystique/springmvc/controller/EmployeeController.java
@@ -1,5 +1,33 @@
 package com.websystique.springmvc.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.websystique.springmvc.model.Employee;
+import com.websystique.springmvc.service.EmployeeService;
+
+@Controller
+@RequestMapping("/")
 public class EmployeeController {
 
+	@Autowired
+	EmployeeService service;
+	
+	@Autowired
+	MessageSource messageSource;
+	
+	@RequestMapping(value = { "/", "/list" }, method = RequestMethod.GET)
+	public String listEmployees(ModelMap model) {
+		List<Employee> employees = service.findAllEmployees();
+		model.addAttribute("employees", employees);
+		return "allemployees";
+	}
+
+	
 }
