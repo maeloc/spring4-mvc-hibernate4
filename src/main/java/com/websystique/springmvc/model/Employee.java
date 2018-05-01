165320845f78fe510c1d4e9ee9c1a162fd8598c4
maeloc <anxogomez@gmail.com>
2018-05-01 13:21:33 +0200

    Added field and default constructor

--- src/main/java/com/websystique/springmvc/model/Employee.java
+++ src/main/java/com/websystique/springmvc/model/Employee.java
@@ -112,6 +112,20 @@ public class Employee {
 		return true;
 	}
 
+	
+	public Employee() {
+		super();
+	}
+
+	public Employee(int id, String name, LocalDate joiningDate, BigDecimal salary, String ssn) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.joiningDate = joiningDate;
+		this.salary = salary;
+		this.ssn = ssn;
+	}
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", joiningDate=" + joiningDate + ", salary=" + salary
